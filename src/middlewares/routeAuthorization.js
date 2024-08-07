9b7828d335ec23e0aee3dd49a776e619e82fe673
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2024-08-07 13:03:56 -0500

    cors URL production

--- src/middlewares/routeAuthorization.js
+++ src/middlewares/routeAuthorization.js
@@ -1,11 +1,8 @@
-
-
 const routeAuthorization = (req, res, next) => {
-  console.log("REQUEST EN ROUTE AUTH: ", req);
   if (req.isAuthenticated()) {
     return next();
   }
   res.status(401).json({ message: "User not signed up" });
 };
 
-export default routeAuthorization
+export default routeAuthorization;
